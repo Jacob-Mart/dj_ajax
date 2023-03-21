b221c4e6ec07be7d18e8e78a710b6d77921367a7
Jacob-Mart <Jmartens3800@conestogac.on.ca>
2023-03-20 21:26:11 -0400

    Added modal forms to update and delete buttons

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,7 +1,11 @@
+const postBox = document.getElementById('post-box')
 const backBtn = document.getElementById('back-btn')
 const updateBtn = document.getElementById('update-btn')
 const deleteBtn = document.getElementById('delete-btn')
 const spinnerBox = document.getElementById('spinner-box')
+
+const titleInput = document.getElementById('id_title')
+const bodyInput = document.getElementById('id_body')
 
 const url = window.location.href + "data/"
 
@@ -13,7 +17,6 @@ $.ajax({
     type: 'GET',
     url: url,
     success: function(response){
-        console.log(response)
         const data = response.data
 
         if(data.logged_in !== data.author){
@@ -23,8 +26,22 @@ $.ajax({
             deleteBtn.classList.remove('not-visible')
         }
 
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-3')
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titleEl.textContent = data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
+
+        titleInput.value = data.title
+        bodyInput.value = data.body
+
         spinnerBox.classList.add('not-visible')
-        
+
     },
     error: function(error){
         console.log(error)
