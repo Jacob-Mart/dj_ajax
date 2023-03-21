5aa96a0c6e52eb2350f9e539e78d8a750f98a20b
Jacob-Mart <Jmartens3800@conestogac.on.ca>
2023-03-20 21:07:33 -0400

    Added hiding of post delete and update buttons if not correct user

--- posts/static/posts/detail.js
+++ posts/static/posts/detail.js
@@ -1,6 +1,9 @@
 const backBtn = document.getElementById('back-btn')
-const url = window.location.href + "data/"
+const updateBtn = document.getElementById('update-btn')
+const deleteBtn = document.getElementById('delete-btn')
 const spinnerBox = document.getElementById('spinner-box')
+
+const url = window.location.href + "data/"
 
 backBtn.addEventListener('click', ()=>{
     history.back()
@@ -11,7 +14,17 @@ $.ajax({
     url: url,
     success: function(response){
         console.log(response)
+        const data = response.data
+
+        if(data.logged_in !== data.author){
+            
+        } else{
+            updateBtn.classList.remove('not-visible')
+            deleteBtn.classList.remove('not-visible')
+        }
+
         spinnerBox.classList.add('not-visible')
+        
     },
     error: function(error){
         console.log(error)
